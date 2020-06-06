08c237dc1caf0b2e1ee1c1cf4477c8e39247211f
KamranKhaan <kamran.dataentry@gmail.com>
2020-06-06 01:10:14 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          | Hello World |
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          | This is a Ball |
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
